6458d456ae6478b59a57927e874549cf7a0ce5e6
Elyon <ElyonOlaniran@outlook.com>
2022-02-17 17:25:49 -0800

    epoch converter working

--- screens/match-details-screen.js
+++ screens/match-details-screen.js
@@ -5,10 +5,15 @@ import { GameDetailsHandler } from '../components/game-details-handler.component
 
 export const GameDetails = ({ route, navigation }) => {
     const { game, result, player } = route.params;
+    const _epochConverter = (secs) => {
+        let minutes = Math.floor(secs/60);
+        let seconds = ((secs % 60)).toFixed(0);
+        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    }
     return(
         <View>
             <Text>{player}</Text>
-            <Text>Game details screen yay</Text>
+            <Text>Total game time: {_epochConverter(result.challenges.gameLength)} </Text>
             <Text style={styles.title}>Winning Team</Text>
             {game.info.participants.filter(participant => participant.win).map( participant =>(
                 <GameDetailsHandler
@@ -25,7 +30,17 @@ export const GameDetails = ({ route, navigation }) => {
                     />
                     ))
                 }
-                <Text>You landed{result.challenges.skillshotsHit} skillshots and dodged {result.challenges.skillshotsDodged}skillshots</Text>
+                <Text>Control Wards Placed: {result.challenges.controlWardsPlaced}</Text>
+                <Text>Trinket Wards Placed: {result.challenges.stealthWardsPlaced}</Text>
+                <Text>Solo Kills: {result.challenges.soloKills}</Text>
+                <Text>Kill participation: {result.challenges.killParticipation.toFixed(2) *10 *10}%</Text>
+                <Text>Percentage of team damage: {result.challenges.teamDamagePercentage.toFixed(3) * 10 * 10}%</Text>
+                <Text>Minions killed in first 10 minutes: {result.challenges.laneMinionsFirst10Minutes}</Text>
+                <Text>Turret plates taken: {result.challenges.turretPlatesTaken}</Text>
+                <Text>Maximum CS advantage over lane opponent: {result.challenges.maxCsAdvantageOnLaneOpponent}</Text>
+                <Text>Maximum level lead on lane opponent: {result.challenges.maxLevelLeadLaneOpponent} </Text>
+                <Text>Skillshots landed: {result.challenges.skillshotsHit} </Text>
+                <Text>Skillshots dodged: {result.challenges.skillshotsDodged} </Text>
         </View>
     );
 
